431d83107edce50f7dda9da3fcf21ec25b31f4a3
Alexandra Stalmans <alexandra.stalmans@student.hogent.be>
2023-11-04 10:22:52 +0100

    Linting errors fixed - build workflow

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((_, res, next) => {
-  res.set("X-Database-Used", process.env.MONGO_URL ? "MongoDB" : "SQLite");
+  res.set('X-Database-Used", process.env.MONGO_URL ? "MongoDB" : "SQLite');
   next();
 });
 
